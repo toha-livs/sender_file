88a645e5ee879585572a59c2dea7d0244e9c048b
toha-livs <aghica@bk.ru>
2019-09-10 16:29:02 +0300

    init

--- sender/urls.py
+++ sender/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
+from django.conf.urls import include, url
 from .views import sender
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('file', sender, name='sender')
+    url(r'^admin/$', admin.site.urls),
+    url(r'^file/$', sender, name='sender')
 ]
